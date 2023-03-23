c02b9203a71e87ce693ccfdc51d1d481f21fcfd4
Leonel Ochoa <leonel170814@gmail.com>
2023-03-23 15:17:07 -0300

    new upload

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,17 +6,17 @@ import { Link} from 'react-router-dom'
 export const Navbar = ({red}) =>{
 
     return (
-        <header className="header" style={{backgroundColor: red ? 'red' : 'darkcyan'}}>
+        <header className="header">
             <div className="header__container">
-                <img src='./imgs/libreriaLogo.png'className="header__logo" alt='logo'></img>
+                <img src='https://triunfacontulibro.com/wp-content/uploads/2016/10/icon-libro.png'className="header__logo" id='img-logo' alt='Logo'></img>
 
                 <nav className="navbar">
                     <Link to='/' className="navbar__link">Inicio</Link>
-                    <Link to='/productos/limpieza' className="navbar__link">Limpieza</Link>
-                    <Link to='/productos/carniceria' className="navbar__link">Carnes</Link>
-                    <Link to='/productos/panaderia' className="navbar__link">Pan</Link>
-                    <Link to='/productos/perfumeria' className="navbar__link">Perfu</Link>
-                    <Link to='/productos/verduleria' className="navbar__link">Verdu</Link>
+                    <Link to='/productos/mochila' className="navbar__link">Mochilas</Link>
+                    <Link to='/productos/cartucheras' className="navbar__link">Cartucheras</Link>
+                    <Link to='/productos/lapiceras' className="navbar__link">Lapiceras</Link>
+                    <Link to='/productos/cuadernos' className="navbar__link">Cuadernos</Link>
+                    <Link to='/productos/borrador' className="navbar__link">Borradores</Link>
                     {/* <Link to='/nosotros' className="navbar__link">Nosotros</Link> */}
                     {/* <Link to='/pokeapi' className="navbar__link">Pokeapi</Link> */}
                 </nav>
